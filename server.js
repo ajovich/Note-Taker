e048d261d5db44b0414b51d93596026c31ec7441
Allysha Jovich <ajovich@hotmail.com>
2021-08-10 16:45:33 -0700

    updated server.js to include routers, added routes folder with apiRoutes.js and htmlRoutes.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Dependencies
 const express = require('express');
-const routes = require('./routes');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 // Sets up Express app
 const app = express();
@@ -10,6 +11,11 @@ const PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
+
+// Routers
+// Give server a map of how to respond when user visits or requests data from various URLs 
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
 
 // Starts the server to begin listening
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
