111f0a53eacf2eae84189860263743292af99cd0
Allysha Jovich <ajovich@hotmail.com>
2021-08-10 18:01:44 -0700

    attempting to fix bug on adding and saving notes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,14 +2,31 @@
 const fs = require('fs');
 const db = require('../db/db.json');
 
+// let noteData = fs.readFileSync('./db/db.json', 'utf8');
+// let notes = JSON.parse(noteData);
+
+// Function whenever a note is added or deleted
+function saveNotes () {
+    fs.writeFile('./db/db.json', JSON.stringify(notes), err => {
+        if (err) throw err;
+        return true;
+    });
+};
+
 module.exports = (app) => {
+    // Sets notes variable
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) throw err;
+        var notes = JSON.parse(data);
+    });
+
     // GET Requests
     app.get('/api/notes', (req, res) => res.json(notes));
 
     // POST Requests
     app.post('/api/notes', (req, res) => {
         notes.push(req.body);
-        res.json(true);
+        saveNotes();
     });
 
     // GET Requests for note with specific id
@@ -21,5 +38,6 @@ module.exports = (app) => {
     // 'splice' method to delete existing note based on specific id
     app.delete('/api/notes/:id', (req, res) => {
         notes.splice(req.params.id, 1);
+        saveNotes();
     });
 };
